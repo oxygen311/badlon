14fca54d610f4a49c05b80992a0d7873494937ba
Alexey Zabelkin <a@zab.one>
2025-06-03 21:42:50 +0100

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='badlon',
-    version='0.1.3',
+    version='0.1.4',
     description='A bioinf tool for analyzing pan-genome and other features based on synteny blocks',
     long_description=long_description,
     long_description_content_type='text/markdown',
